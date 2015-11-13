35376890c62ce11bacf2afa239949edaa763cd33
Xavier Fornes <xavier.fornes@scmspain.com>
2015-11-13 17:22:10 +0100

    Added tests for "Guice module" style of using healthcheck endpoint module

---
+++ src/test/java/com/scmspain/karyon/healthcheckendpoint/module/HealthCheckControllerTest.java
@@ -0,0 +1,21 @@
+package com.scmspain.karyon.healthcheckendpoint.module;
+
+import com.scmspain.karyon.healthcheckendpoint.module.AbstractControllerTest;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.reactivex.netty.protocol.http.client.HttpClientRequest;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ */
+public class HealthCheckControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void itShouldBeHealthy() throws Exception {
+
+    createHttpClient()
+                .submit(HttpClientRequest.createGet("/healthcheck"))
+                .doOnNext(response -> Assert.assertEquals(HttpResponseStatus.OK, response.getStatus()))
+                .toBlocking().single();
+    }
+}
---
+++ src/test/java/com/scmspain/karyon/healthcheckendpoint/module/server/AppServerForTesting.java
@@ -0,0 +1,32 @@
+package com.scmspain.karyon.healthcheckendpoint.module.server;
+
+import com.google.inject.Singleton;
+import com.netflix.governator.annotations.Modules;
+import com.scmspain.karyon.healthcheckendpoint.module.HealthCheckEndPointModule;
+import netflix.karyon.KaryonBootstrap;
+import netflix.karyon.health.AlwaysHealthyHealthCheck;
+import scmspain.karyon.restrouter.KaryonRestRouterModule;
+
+@KaryonBootstrap(name = "AppServer", healthcheck = AlwaysHealthyHealthCheck.class)
+@Singleton
+@Modules(include = {
+    AppServerForTesting.KaryonRestRouterModuleImpl.class
+})
+public interface AppServerForTesting {
+    class KaryonRestRouterModuleImpl extends KaryonRestRouterModule {
+
+        public static final int DEFAULT_PORT = 8000;
+        public static final int DEFAULT_THREADS = 20;
+
+        @Override
+        protected void configureServer() {
+            server().port(DEFAULT_PORT).threadPoolSize(DEFAULT_THREADS);
+        }
+
+        @Override
+        public void configure()
+        {
+            super.configure();
+        }
+    }
+}
